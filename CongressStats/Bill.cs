1fa514c1e2b8db37d6786aae16b2a55572dd54ac
codarobe <codarobe@ut.utm.edu>
2018-03-29 13:46:34 -0500

    Add scraped data for 115th congress and create sample files for parsing into database

---
+++ CongressStats/Bill.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * File: Bill.cs
+ * Description: Class definition for deserializing bill object from congress xml files
+ * Author: Cody Robertson
+ * Date: 3/29/18
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongressStats
+{
+    class Bill
+    {
+        // define fiels from xml
+    }
+}
---
+++ CongressStats/BillParser.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * File: BillParser.cs
+ * Description: Class definition to automate parsing bills from xml files
+ * Author: Cody Robertson
+ * Date: 3/29/18
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongressStats
+{
+    class BillParser
+    {
+        public List<Bill> ParseBills(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
