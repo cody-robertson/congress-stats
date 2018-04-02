8fb112093c341bc99cb082bff6fa7475705d4420
codarobe <codarobe@ut.utm.edu>
2018-04-02 10:32:55 -0500

    Added some initial files for deserializing xml

---
+++ CongressStats/Deserialization/Bill/Bill.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * File: Bill.cs
+ * Description: Class definition for deserializing bill object from congress xml files
+ * Author: Cody Robertson
+ * Date: 3/29/18
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongressStats.Deserialization.Bill
+{
+    [XmlRoot(ElementName = "bill", Namespace = "")]
+    class Bill
+    {
+        // define fields from xml
+
+        [XmlElementAttribute("introducedDate")]
+        public string introducedDate;
+
+        [XmlElementAttribute("congress")]
+        public uint congressNumber;
+
+        [XmlElementAttribute("actions")]
+        public BillAction billAction;
+
+
+    }
+}
